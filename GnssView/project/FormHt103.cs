d47a7e76c8195adc0ead00dd554dc67340d503e0
Administrator <Administrator@WIN-7NE1BQENP7G>
2021-02-24 15:39:20 +0800

    版本：2.5.0.2 时间：20210224  1、优化ht103

--- GnssView/project/FormHt103.cs
+++ GnssView/project/FormHt103.cs
@@ -39,29 +39,29 @@ namespace GnssView.project
                     label10.Text = data[3].ToString();
                     label14.Text = BitConverter.ToUInt16(data, 4).ToString();
                     label18.Text = data[6].ToString();
-                    label22.Text = (BitConverter.ToSingle(data, 7) / 0.001).ToString();
-                    label26.Text = BitConverter.ToInt16(data, 11).ToString();
+                    label22.Text = (BitConverter.ToSingle(data, 7) * 0.001).ToString();
+                    label26.Text = BitConverter.ToUInt16(data, 11).ToString();
                     label4.Text = data[13].ToString();
                     label8.Text = data[14].ToString();
                     label12.Text = data[15].ToString();
-                    label16.Text = (BitConverter.ToInt32(data, 16) / 1000.0).ToString("f3");
+                    label16.Text = (BitConverter.ToUInt32(data, 16) / 1000.0).ToString("f3");/*秒*/
                     label20.Text = data[20].ToString();
                     label24.Text = data[21].ToString();
-                    label28.Text = BitConverter.ToInt16(data, 22).ToString();
-                    label30.Text = data[24].ToString();
+                    label28.Text = BitConverter.ToUInt16(data, 22).ToString();
+                    label30.Text = ((data[24]>>4) & 0x3).ToString() + "/" + ((data[24]>>2) & 0x3).ToString() + "/" + (data[24]&0x3).ToString();
                     label32.Text = data[25].ToString();
                     label34.Text = data[26].ToString();
                     label36.Text = data[27].ToString();
-                    label38.Text = (BitConverter.ToInt32(data, 28) * 1e-7).ToString("f8");
-                    label40.Text = (BitConverter.ToInt32(data, 32) * 1e-7).ToString("f8");
-                    label42.Text = (BitConverter.ToInt32(data, 36) * 1e-7).ToString("f8");
-                    label44.Text = (BitConverter.ToInt32(data, 40) * 1e-7).ToString("f8");
-                    label46.Text = (BitConverter.ToInt32(data, 44) * 1e-7).ToString("f8");
-                    label48.Text = (BitConverter.ToInt32(data, 48) * 1e-7).ToString("f8");
-                    label50.Text = (BitConverter.ToInt16(data, 52) * 0.01).ToString("f8");
-                    label52.Text = (BitConverter.ToInt16(data, 54) * 0.01).ToString("f8");
-                    label54.Text = (BitConverter.ToInt16(data, 56) * 0.01).ToString("f8");
-                    label56.Text = (BitConverter.ToInt16(data, 58) * 0.1).ToString("f8");
+                    label38.Text = (BitConverter.ToInt32(data, 28) * 1.0e-7).ToString("f8");
+                    label40.Text = (BitConverter.ToInt32(data, 32) * 1.0e-7).ToString("f8");
+                    label42.Text = (BitConverter.ToInt32(data, 36) * 1.0e-2).ToString("f8");
+                    label44.Text = (BitConverter.ToInt32(data, 40) * 1.0e-2).ToString("f8");
+                    label46.Text = (BitConverter.ToInt32(data, 44) * 1.0e-2).ToString("f8");
+                    label48.Text = (BitConverter.ToInt32(data, 48) * 1.0e-2).ToString("f8");
+                    label50.Text = (BitConverter.ToUInt16(data, 52) * 0.01).ToString("f8");
+                    label52.Text = (BitConverter.ToUInt16(data, 54) * 0.01).ToString("f8");
+                    label54.Text = (BitConverter.ToUInt16(data, 56) * 0.01).ToString("f8");
+                    label56.Text = (BitConverter.ToUInt16(data, 58) * 0.1).ToString("f8");
                 }));
             }
             catch { }
